b9a6de3c2303896f8826b43cf584e449916803dd
Ryder Retzlaff <retzerrt@gmail.com>
2023-01-16 08:28:24 +1100

    Added test jpg

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,16 @@
 """Test the main function
 """
 
+import requests
 from src import main as py_meta
 from src.internals.metadata import MetaData
+
+image = requests.get(
+    "https://file-examples.com/storage/fea8fc38fd63bc5c39cf20b/2017/10/file_example_JPG_500kB.jpg",
+    timeout=6,
+)
+with open("demo_file.jpg", "wb") as f:
+    f.write(image.content)
 
 
 def test_read():
